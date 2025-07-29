88d295b1508acdbbb3762c0e4ec87b37751e0f61
IonutG2001 <ionut.gheba@creativemed.ro>
2025-07-29 10:52:25 +0300

    fix blank page

--- client/src/components/ToastProvider.tsx
+++ client/src/components/ToastProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type { ReactNode } from "react";
-import { ToastProvider as InternalToastProvider } from "@/hooks/use-toast/use-toast-hook";
+import { ToastProvider as InternalToastProvider } from "@/hooks/use-toast/useToast";
 
 interface ToastProviderProps {
   children: ReactNode;
