240c4cc35893f4a41886e58b9093f85ad54261f8
IonutG2001 <ionut.gheba@creativemed.ro>
2025-07-29 15:28:42 +0300

    fix

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,4 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/?(*.)+(test).[tj]s'],
+  moduleNameMapper: {
+    '^@lib/(.*)$': '<rootDir>/src/lib/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
+    '^@validators/(.*)$': '<rootDir>/src/validators/$1',
+  },
 };
